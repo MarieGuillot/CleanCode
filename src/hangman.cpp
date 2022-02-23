8049b8156eb7366a6a3b5622a27a3d0d8e0a0dae
Marie <guillot-marie@hotmail.fr>
2022-02-23 17:27:09 +0100

    [hangman] getOneLetterFromUser

--- src/hangman.cpp
+++ src/hangman.cpp
@@ -1,5 +1,6 @@
 #include "hangman.hpp"
 #include <array>
+#include <cctype>
 #include <iostream>
 #include <string>
 #include "random.hpp"
@@ -15,11 +16,33 @@ std::string chooseARandomWord()
     };
 
     const auto wordPosition = giveRandomIntegerNumber<size_t>(0, numberOfWords - 1);
-    return words[wordPosition];
+    return words.at(wordPosition);
+}
+
+std::string getOneLetterFromUser()
+{
+    std::string playerLetter = "a";
+    std::cout << "Give me a letter" << std::endl;
+    while (!(std::cin >> playerLetter)) {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Give me a letter, please." << std::endl;
+    }
+    if (playerLetter.length() > 1) {
+        std::cout << "You can try only one letter." << std::endl;
+        return getOneLetterFromUser();
+    }
+    if (!isalpha(int(playerLetter.at(0)))) {
+        std::cout << "Give me a letter, please." << std::endl;
+        return getOneLetterFromUser();
+    }
+    return playerLetter;
 }
 
 void playHangman()
 {
-    std::string myWord = chooseARandomWord();
-    std::cout << myWord << std::endl;
+    std::string solutionWord = chooseARandomWord();
+    std::cout << solutionWord << std::endl;
+    std::string playerLetter = getOneLetterFromUser();
+    std::cout << playerLetter << std::endl;
 }
